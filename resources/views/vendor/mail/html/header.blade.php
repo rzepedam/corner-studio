dccba41582a6153ce7ee4886a273a0a55d692c71
Raúl Elías <rauleliasmezamora@gmail.com>
2017-06-05 13:04:55 -0400

    xxx

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}">
         	<div class="vertical-align">
-            	<img src="{{ asset('/img/logo_full.png') }}" height="40px" width="65px">
+            	<img src="{{ asset('/img/logo_full.png') }}" height="50px" width="75px">
         	</div>
         </a>
     </td>
